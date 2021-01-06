04391791a22a04938fe7ee39f69e1f9cbd3f01b2
kordian127 <kordian126@gmail.com>
2021-01-06 19:35:47 +0100

    Done

--- GenomicRangeQuery.cpp
+++ GenomicRangeQuery.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 int main(){
     int m=3;
+    int res,war=5;
+    vector <int> result;
     int p[m], q[m];
     //A=1 C=2 G=3 T=4
     string s;
@@ -20,10 +23,32 @@ int main(){
         for(int j=p[i];j<=q[i];j++){
             char znak=s[j];
             cout<<znak;
+            switch (znak){
+        case 'A':
+            res=1;
+            break;
+        case 'C':
+            res=2;
+            break;
+        case 'G':
+            res=3;
+            break;
+        case 'T':
+            res=4;
+            break;
+            }
+            if(res<war){
+            war=res;
+            }
         }
+        result.push_back(war);
         cout<<endl;
+        war=5;
     }
 
+    for(int x=0;x<m;x++){
+        cout<<result[x];
+    }
 
 
 return 0;
